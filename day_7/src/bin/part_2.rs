0978c560f3b0fbe91781820f872aab4006e7337d
s-jad <s_jad@web.de>
2024-01-05 17:53:18 +0100

    Feat: solved part 2 day 7

---
+++ day_7/src/bin/part_2.rs
@@ -0,0 +1,74 @@
+use itertools::Itertools;
+use std::{collections::HashMap, time::Instant};
+
+fn dfs(
+    graph: &HashMap<String, Vec<(usize, String)>>,
+    bag: &str,
+    visited: &mut HashMap<String, bool>,
+    count: &mut usize,
+    multiplier: usize,
+) -> bool {
+    if bag != "shiny gold" {
+        *count += multiplier;
+    }
+
+    if let Some(bags) = graph.get(bag) {
+        for (n, b) in bags {
+            dfs(graph, b, visited, count, *n * multiplier);
+        }
+    }
+
+    false
+}
+
+fn process(input: &str) -> usize {
+    let graph = input
+        .lines()
+        .map(|l| {
+            let (bag, c) = l
+                .split_terminator(" bags contain ")
+                .collect_tuple::<(&str, &str)>()
+                .unwrap();
+
+            if c == "no other bags." {
+                (bag.to_owned(), vec![])
+            } else {
+                let contents = c.split_terminator(", ").collect_vec();
+                let mut content_vec = vec![];
+                for content in contents {
+                    let parts = content.split_once(" ").unwrap();
+                    let num = parts.0.parse::<usize>().unwrap();
+                    let description = parts
+                        .1
+                        .trim_end_matches(" bag")
+                        .trim_end_matches(" bags")
+                        .trim_end_matches(" bag.")
+                        .trim_end_matches(" bags.")
+                        .to_owned();
+
+                    content_vec.push((num, description));
+                }
+
+                (bag.to_owned(), content_vec)
+            }
+        })
+        .collect::<HashMap<String, Vec<(usize, String)>>>();
+
+    let mut count: usize = 0;
+    let mut visited: HashMap<String, bool> = HashMap::new();
+    let multiplier = 1;
+
+    dfs(&graph, "shiny gold", &mut visited, &mut count, multiplier);
+
+    count
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
